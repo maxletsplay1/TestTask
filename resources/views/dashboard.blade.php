48d9d50d7c99315ad60eca00721e2777239ad998
Maksik <123letsplay93@gmail.com>
2024-08-30 22:11:10 +0700

    FIX: dropdown fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,8 +17,8 @@
             <div class="flex flex-col mt-6">
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                        <div class="overflow-hidden border border-gray-200 md:rounded-lg">
-                            <table class="min-w-full divide-y divide-gray-200">
+                        <div class="overflow-auto border border-gray-200 md:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200 mb-32">
                                 <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col"
